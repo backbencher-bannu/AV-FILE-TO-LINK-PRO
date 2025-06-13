0be34c1bb44b1e77c76fe9ba701be95da7b5d3ff
backbencher-bannu <harishwhatsapp0306@gmail.com>
2025-06-13 12:39:36 +0530

    Update info.py

--- info.py
+++ info.py
@@ -52,7 +52,7 @@ BAN_ALERT = environ.get('BAN_ALERT' , '<b>ʏᴏᴜʀ ᴀʀᴇ ʙᴀɴɴᴇᴅ �
 
 # MongoDB information
 DATABASE_URI = environ.get('DATABASE_URI', "mongodb+srv://harishwhatsapp0306:kumarbannu@stream.6tn3cpm.mongodb.net/?retryWrites=true&w=majority&appName=stream")
-DATABASE_NAME = environ.get('DATABASE_NAME', "cluster0")
+DATABASE_NAME = environ.get('DATABASE_NAME', "stream")
 
 # fsub  information
 AUTH_PICS = environ.get('AUTH_PICS', 'https://envs.sh/AwV.jpg')              
